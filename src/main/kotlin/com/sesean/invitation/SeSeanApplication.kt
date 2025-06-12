119003554b2b29383defc2f15701d5259af0b95d
devjune <dev.sjune@gmail.com>
2025-06-12 17:25:06 +0900

    chore: initial setup

---
+++ src/main/kotlin/com/sesean/invitation/SeSeanApplication.kt
@@ -0,0 +1,11 @@
+package com.sesean.invitation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeSeanApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeSeanApplication>(*args)
+}
---
+++ src/test/kotlin/com/sesean/invitation/SeSeanApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.sesean.invitation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SeSeanApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
